5480403ffbcdb4e4701322ae3025e2904a88b552
Nathan <nathan.stuber.a@gmail.com>
2023-02-23 20:48:58 -0300

    feat: primeira possibilidade de padrão

--- src/routes/key-user-finder/controller/keyUsers.js
+++ src/routes/key-user-finder/controller/keyUsers.js
@@ -1,7 +1,17 @@
-module.exports.index = function(application, req, res) {
-    var newsModel = new application.src.routes.key-user-finder.models.news();
-  
-    newsModel.getLastNews(function(err, result) {
-      res.render("news/index", {news : result});
-    });
-  }
+module.exports.keyUsers = (req, res, next) => {
+  const products = [
+      {
+        id: 1,
+        name: "hammer",
+      },
+      {
+        id: 2,
+        name: "screwdriver",
+      },
+      {
+        id: 3, 
+        name: "wrench",
+      },
+     ];
+  res.json(products);
+}
--- src/routes/key-user-finder/model/keyUsers.js
+++ src/routes/key-user-finder/model/keyUsers.js
@@ -1,23 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const isAuthorized = require("../../auth.js");
+const controller = require("../controller/keyUsers.js");
 
-router.get("/keyUsers", isAuthorized, (req, res, next) => {
-    const products = [
-        {
-          id: 1,
-          name: "hammer",
-        },
-        {
-          id: 2,
-          name: "screwdriver",
-        },
-        {
-          id: 3, 
-          name: "wrench",
-        },
-       ];
-    res.json(products);
-})
+router.get("/keyUsers", isAuthorized, controller.keyUsers)
+router.post("/keyUsers", isAuthorized, controller.keyUsers)
 
 module.exports = router;
